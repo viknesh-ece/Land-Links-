c3594d1a2988768d058f95c295ab84b336882104
Your Name <you@example.com>
2026-06-25 14:14:53 +0530

    Add new files

---
+++ src/app/api/properties/[id]/route.ts
@@ -0,0 +1,74 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+// DELETE PROPERTY
+export async function DELETE(
+req: Request,
+{ params }: { params: Promise<{ id: string }> }
+): Promise<Response> {
+try {
+const { id } = await params;
+await prisma.property.delete({
+where: {
+id: Number(id),
+},
+});
+
+
+return NextResponse.json({
+  message: "Property deleted successfully",
+});
+
+
+} catch (error) {
+console.log(error);
+
+return NextResponse.json(
+  { message: "Server Error" },
+  { status: 500 }
+);
+
+
+}
+}
+
+// UPDATE PROPERTY
+export async function PUT(
+req: Request,
+{ params }: { params: Promise<{ id: string }> }
+): Promise<Response> {
+try {
+const { id } = await params;
+const body = await req.json();
+
+
+const property = await prisma.property.update({
+  where: {
+    id: Number(id),
+  },
+  data: {
+    title: body.title,
+    description: body.description,
+    price: Number(body.price),
+    location: body.location,
+  },
+});
+
+return NextResponse.json({
+  message: "Property updated successfully",
+  property,
+});
+
+
+} catch (error) {
+console.log(error);
+
+
+return NextResponse.json(
+  { message: "Server Error" },
+  { status: 500 }
+);
+
+
+}
+}
